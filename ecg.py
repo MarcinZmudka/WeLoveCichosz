05556c983ccfa4c1f3e1d25120ae14d32339780e
unknown <marcinzmudka@gmail.com>
2020-04-14 14:59:04 +0200

    import data from files rewrited

--- ecg.py
+++ ecg.py
@@ -3,27 +3,37 @@ import matplotlib.pyplot as plt
 from biosppy import storage
 from biosppy.signals import ecg
 from math import ceil
+import librosa
+import pandas as pd
+
 
 def import_from_txt(path_to_file, fs):
-    signal, mdata = storage.load_txt(f"{path_to_file}")
-    samples = [[] for i in range(0,12)]
-    for sig in signal:
-        splited = sig.decode("ASCII").split(" ")
-        for i in range(0,12):
-            samples[i].append(int(splited[i]))
-    N = len(samples[0])  # number of samples
+    # signal, mdata = storage.load_txt(f"{path_to_file}")
+    try:
+        data = pd.read_csv(f"{path_to_file}", sep=" ", header=None)
+    except:
+        data = pd.read_csv(f"{path_to_file}", sep="  ", header=None)
+    rows, number_of_columns = data.shape
+    if number_of_columns == 2:  # if second column is time
+        return [data[0], [data[1]]]
+
+    N = rows  # number of samples
     T = (N - 1) / fs  # duration
     ts = np.linspace(0, T, N, endpoint=False)  # relative timestamps
-    make_chart(ts, samples, "czas", "y")
+    return [ts, data]
 
 
 def make_chart(x, y, xlabel, ylabel):
-    number_of_charts = len(y)
-    print(number_of_charts)
+    try:
+        rows, number_of_charts = y.shape
+    except:
+        number_of_charts = 1
     rows = ceil(number_of_charts/2)
-    fig, axs = plt.subplots(rows ,2)
+    fig, axs = plt.subplots(rows, 2)
+    if rows == 1:
+        axs = [axs]
     index_of_plot = 0
-    for i in range(0,rows):
+    for i in range(0, rows):
         if i == (rows - 1) and number_of_charts % 2 == 1:
             axs[i][0].plot(x, y[index_of_plot], lw=1)
             axs[i][0].set_title(f"kanał: {index_of_plot}")
@@ -31,14 +41,10 @@ def make_chart(x, y, xlabel, ylabel):
             index_of_plot += 1
             axs[i][0].grid()
         else:
-            for j in range(0,2):
+            for j in range(0, 2):
                 axs[i][j].plot(x, y[index_of_plot], lw=1)
                 axs[i][j].set_title(f"kanał: {index_of_plot}")
                 axs[i][j].set_xlabel("T[s]")
                 index_of_plot += 1
                 axs[i][j].grid()
     plt.show()
-
-
-
-    #legenda, liczba próbek, wartość średnia
--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import ecg as ecg
 
-ecg.import_from_txt("./ekg2.txt", 800.)
+[ts, samples] = ecg.import_from_txt("./ekg100.txt", 360.)
+# ts - timestamp
+# samples - wartość próbki
+ecg.make_chart(ts, samples, "czas", "wartość sygnału")
+
+# x = ecg.spectrum(samples)
+# ecg.make_chart(ts, x, "czas", "wartość sygnału")
